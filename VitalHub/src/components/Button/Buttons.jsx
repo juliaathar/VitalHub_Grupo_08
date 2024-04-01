feb0e960f76e688d3bdd8216c4352ffed551442f
juliaathar <juathar@gmail.com>
2024-04-01 09:07:03 -0300

    :sparkles: Add carregamento no botão de login
    Botão é desativado por três segundos após tentativa falha de login, função feita pra evitar requisições desnecessárias pra API

--- VitalHub/src/components/Button/Buttons.jsx
+++ VitalHub/src/components/Button/Buttons.jsx
@@ -1,28 +1,19 @@
-import { TouchableHighlight } from "react-native"
-import { Button, ButtonTitle, ButtonGoogle, ButtonGoogleTitle } from "./Style"
-// import { AntDesign } from '@expo/vector-icons';
+import React from 'react';
+import { ActivityIndicator, TouchableOpacity } from 'react-native';
+import { Button, ButtonTitle, ButtonGoogle, ButtonGoogleTitle } from './Style';
 
-export const NormalButton = ({
-    title,
-    fieldWidth = 100,
-    onPress
-}) => {
-    return(
-        <Button fieldWidth={fieldWidth} onPress={onPress}>
-            <ButtonTitle>{title}</ButtonTitle>
-        </Button>
-    )
-}
+export const NormalButton = ({ title, fieldWidth = 100, onPress, disabled = false }) => {
+  return (
+    <Button fieldWidth={fieldWidth} onPress={onPress} disabled={disabled}>
+      {disabled ? <ActivityIndicator color="white" /> : <ButtonTitle>{title}</ButtonTitle>}
+    </Button>
+  );
+};
 
-export const GoogleButton = ({
-    title,
-    fieldWidth,
-    onPress
-}) => {
-    return(
-        <ButtonGoogle fieldWidth={fieldWidth} onPress={onPress}>
-            {/* <AntDesign name="google" size={24} color="#496BBA" /> */}
-            <ButtonGoogleTitle>{title}</ButtonGoogleTitle>
-        </ButtonGoogle>
-    )
-}
+export const GoogleButton = ({ title, fieldWidth, onPress }) => {
+  return (
+    <ButtonGoogle fieldWidth={fieldWidth} onPress={onPress}>
+      <ButtonGoogleTitle>{title}</ButtonGoogleTitle>
+    </ButtonGoogle>
+  );
+};
--- VitalHub/src/screens/Login/Login.jsx
+++ VitalHub/src/screens/Login/Login.jsx
@@ -1,70 +1,66 @@
-import { ContentAccount, TextAccount, TextAccountLink } from "../../components/ContentAccount/Style"
-import { GoogleButton, NormalButton } from "../../components/Button/Buttons"
-import AsyncStorage from "@react-native-async-storage/async-storage"
-import { Container } from "../../components/Container/Style"
-import { LinkMedium } from "../../components/Links/Style"
-import { Alert, TouchableOpacity } from "react-native"
-import { Input } from "../../components/Input/Style"
-import { Title } from "../../components/Title/Style"
-import { Logo } from "../../components/Logo/Style"
-import { useState } from "react"
-
-import api from "../../service/service"
+import React, { useState } from 'react';
+import { Alert, TouchableOpacity, ActivityIndicator } from 'react-native';
+import { Container } from '../../components/Container/Style';
+import { ContentAccount, TextAccount, TextAccountLink } from '../../components/ContentAccount/Style';
+import { GoogleButton, NormalButton } from '../../components/Button/Buttons';
+import { Input } from '../../components/Input/Style';
+import { Logo } from '../../components/Logo/Style';
+import { Title } from '../../components/Title/Style';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import api from '../../service/service';
+import{LinkMedium} from '../../components/Links/Style'
 
 export const Login = ({ navigation }) => {
+  const [email, setEmail] = useState('');
+  const [senha, setSenha] = useState('');
+  const [desativado, setDesativado] = useState(false);
 
-    const [email, setEmail] = useState('julia@gmail.com');
-    const [senha, setSenha] = useState('julia');
+  const handleLogin = async () => {
+    try {
+      if (!desativado) {
+        const response = await api.post('http://172.16.39.82:4466/api/Login', {
+          email: email,
+          senha: senha
+        });
 
-    async function Login() {
-        try {
-            // const response = await api.post('http://172.16.39.82:4466/api/Login', {
-            //     email: email,
-            //     senha: senha
-            // });
+        await AsyncStorage.setItem('token', JSON.stringify(response.data));
 
-            // await AsyncStorage.setItem('token', JSON.stringify(response.data)); //costumava ter .data depois de response
+        navigation.replace('Main');
+      }
+    } catch (error) {
+      console.error('Erro ao fazer login:', error);
 
-            navigation.replace("Main");
-        } catch (error) {
-            console.error('Erro ao fazer login:', error.response.data);
-            Alert.alert('Erro ao fazer login', error.response.data.message || 'Verifique seus dados e tente novamente.');
-        }
+      setDesativado(true);
+
+      setTimeout(() => {
+        setDesativado(false);
+      }, 3000);
     }
-   
+  };
 
-    return (
-        <Container>
-
-            <Logo source={require("../../assets/VitalHub_Logo1.png")} />
-
-            <Title>Entrar ou criar conta</Title>
-
-            <Input
-                placeholder="Usuário ou Email"
-                value={email}
-                onChangeText={(newValue) => { setEmail(newValue) }}
-            />
-
-            <Input
-                placeholder="Senha"
-                secureTextEntry
-                value={senha}
-                onChangeText={(newValue) => { setSenha(newValue) }}
-            />
-
-            <TouchableOpacity style={{ width: "100%"}} onPress={() => navigation.navigate('RecuperarSenha')}>
-                <LinkMedium>Esqueceu sua senha?</LinkMedium>
-            </TouchableOpacity>
-
-            <NormalButton title={"Entrar"} fieldWidth={90} onPress={() => Login()} />
-
-            <GoogleButton title={"Entrar com Google"} fieldWidth={90} />
-
-            <ContentAccount onPress={() => navigation.navigate('CriarConta')}>
-                <TextAccount>Nao tem conta? <TextAccountLink>Crie uma conta agora!</TextAccountLink></TextAccount>
-            </ContentAccount>
-
-        </Container>
-    )
-}
+  return (
+    <Container>
+      <Logo source={require('../../assets/VitalHub_Logo1.png')} />
+      <Title>Entrar ou criar conta</Title>
+      <Input
+        placeholder="Usuário ou Email"
+        value={email}
+        onChangeText={(newValue) => { setEmail(newValue) }}
+      />
+      <Input
+        placeholder="Senha"
+        secureTextEntry
+        value={senha}
+        onChangeText={(newValue) => { setSenha(newValue) }}
+      />
+      <TouchableOpacity style={{ width: "100%" }} onPress={() => navigation.navigate('RecuperarSenha')}>
+        <LinkMedium>Esqueceu sua senha?</LinkMedium>
+      </TouchableOpacity>
+      <NormalButton title={"Entrar"} fieldWidth={90} onPress={handleLogin} disabled={desativado} />
+      <GoogleButton title={"Entrar com Google"} fieldWidth={90} onPress={() => console.log('Entrar com Google')} />
+      <ContentAccount onPress={() => navigation.navigate('CriarConta')}>
+        <TextAccount>Não tem conta? <TextAccountLink>Crie uma conta agora!</TextAccountLink></TextAccount>
+      </ContentAccount>
+    </Container>
+  );
+};
