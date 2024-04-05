14604a04bc48827386c9493509d511a84e64fcec
juliaathar <juathar@gmail.com>
2024-04-05 09:22:50 -0300

    :sparkles: Atualização na table de Endereco
    Feito atualização na Context e criado estrutura pra entidade de Endereco

--- api_v2/WebAPI/obj/Debug/net7.0/WebAPI.AssemblyInfo.cs
+++ api_v2/WebAPI/obj/Debug/net7.0/WebAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+15ba6ae002205d3d72daa38498e7969abc69237f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9eb235d175befccd6125db31964a0f8d1919268e")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
