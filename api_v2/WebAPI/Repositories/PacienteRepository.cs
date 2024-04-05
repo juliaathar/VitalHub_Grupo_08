3ea0b60c5636bc73eb85f01d97dece9e2e10a077
juliaathar <juathar@gmail.com>
2024-04-05 10:48:48 -0300

    :construction: Feito Include de entidades ao listar pacientes e médicos

--- api_v2/WebAPI/Repositories/PacienteRepository.cs
+++ api_v2/WebAPI/Repositories/PacienteRepository.cs
@@ -50,7 +50,7 @@ namespace WebAPI.Repositories
         {
            return ctx.Consultas
                 .Include(x => x.Situacao)
-
+                .Include(x => x.MedicoClinica!.Medico!.Especialidade)
                 //.Where(x  => x.PacienteId == idPaciente && x.DataConsulta == dataConsulta) 
                 .Where(x => x.PacienteId == idPaciente && EF.Functions.DateDiffDay(x.DataConsulta, dataConsulta) == 0)
                 .ToList();
--- api_v2/WebAPI/obj/Debug/net7.0/WebAPI.AssemblyInfo.cs
+++ api_v2/WebAPI/obj/Debug/net7.0/WebAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9eb235d175befccd6125db31964a0f8d1919268e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+14604a04bc48827386c9493509d511a84e64fcec")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
