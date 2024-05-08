f005ce4bcf40a9eafa68c12b1362979fb6719369
juliaathar <juathar@gmail.com>
2024-05-08 08:16:01 -0300

    :wrench: config files

--- api_v2/WebAPI/Utils/Mail/IEmailService.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace WebAPI.Utils.Mail
-{
-    public interface IEmailService
-    {
-        //metodo assincrono para envio de email que recebe o objeto MailRequest
-        Task SendEmailAsync(MailRequest mailRequest);
-    }
-}
